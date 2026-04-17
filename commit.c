fcb2a6ad8ceceaa8e2772c030828c211186488a2
thrupthigowda-1 <thrupthigowda912@gmail.com>
2026-04-17 22:13:59 +0530

    Phase 4: author and timestamp

--- commit.c
+++ commit.c
@@ -179,7 +179,7 @@ int head_update(const ObjectID *new_commit) {
     return rename(tmp_path, target_path);
 }
 
-// ─── TODO: Implement these ──────────────────────────────────────────
+// ─── TODO: Implement these ─────────────────
 
 // Create a new commit from the current staging area.
 //
